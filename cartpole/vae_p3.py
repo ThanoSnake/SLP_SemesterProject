01df629983267ea326aa1863047669909613488c
ntua-el22019 <el22019@mail.ntua.gr>
2026-06-28 12:22:08 +0300

    comment out the main in vae_p3.py

--- cartpole/vae_p3.py
+++ cartpole/vae_p3.py
@@ -246,7 +246,7 @@ def physical_rmse(model, loader, device, std4):
         n += st.size(0)
     return torch.sqrt(se / n).cpu().numpy()
 
-
+"""
 #
 #  Main
 #
@@ -313,3 +313,4 @@ if __name__ == "__main__":
 
     torch.save(model.state_dict(), os.path.join(SAVE_DIR, "vae_last.pth"))
     print("Best val score:", best_val)
+  """
