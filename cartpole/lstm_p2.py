738ff3eaa3683f26f618514c35696dc54efb5f45
ntua-el22019 <el22019@mail.ntua.gr>
2026-06-28 12:18:10 +0300

    comment out the main in lstm_p2.py

--- cartpole/lstm_p2.py
+++ cartpole/lstm_p2.py
@@ -159,7 +159,7 @@ def eval_epoch(model, loader, device, std4, desc=""):
         n += preds.size(0)
     return (se / n).mean(dim=1).cpu().numpy()
 
-
+"""
 # ---------------------------------------------------------------------------
 # Main
 # ---------------------------------------------------------------------------
@@ -237,3 +237,4 @@ if __name__ == "__main__":
     torch.save(model.state_dict(),
                os.path.join(SAVE_DIR, "lstm_p2_alt_last.pth"))
     print("Best val phys-MSE:", best)
+ """
